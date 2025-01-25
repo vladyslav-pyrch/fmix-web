bc692298cd7b0030540ad3fa1f3758bf22f2d6cc
Vladyslav Pyrch <vlad.nuron@gmail.com>
2025-01-25 01:36:06 +0100

    Title changed

--- src/registration.php
+++ src/registration.php
@@ -70,8 +70,9 @@ render:
 
 <head>
 	<meta charset="UTF-8">
-	<title>Registrácia</title>
+	<title>FMIX - Registrácia</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="images/FMIX2.png">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
 	      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
