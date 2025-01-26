ed93c9933e53ecbe13c7241143960c85a2933f78
Vladyslav Pyrch <vlad.nuron@gmail.com>
2025-01-26 16:14:00 +0100

    changed layout

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,26 @@
-﻿function toggleOnSection(id) {
+﻿const delay = ms => new Promise((res) => setTimeout(res, ms))
+
+async function scrollToAndToggleOn(id) {
     const section_contents = document.querySelectorAll("[id*='_content']")
+    const toggle_section_content = document.getElementById(`${id}_content`)
+    const turn_on = toggle_section_content.classList.contains('hide')
     
-    section_contents.forEach(section_content => {
-        if (section_content.classList.contains('show')) {
-            section_content.classList.remove('show');
-            setTimeout(() => sectionContent.classList.add('hide'), 100);
-        } 
+    for (const section of section_contents) {
+        if (section.classList.contains('show')) {
+            section.classList.remove('show')
+            await delay(200)
+            section.classList.add('hide')
+        }
+    }
+
+    if (!turn_on) {
+        return;
+    }
+    
+    toggle_section_content.classList.remove('hide')
+    await delay(200)
+    toggle_section_content.classList.add('show')
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
     })
-    const sectionContent = document.getElementById(id);
-    setTimeout(() => {
-        sectionContent.classList.remove('hide');
-        setTimeout(() => sectionContent.classList.add('show'), 100);
-    }, 100)
 }
