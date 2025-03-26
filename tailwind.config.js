a89f73f241625609a4fc1b87fd804db3be1ba835
Vladyslav Pyrch <vlad.nuron@gmail.com>
2025-03-26 17:47:43 +0100

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
 		extend: {
 			colors: {
 				cobalt: '#053da8',
+				'light-violet':'#8a61e7',
 				'purple-heart': '#5831ac',
 				'violet-eggplant': '#a706a6',
 				'catalina-blue': '#042b76',
