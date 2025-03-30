b133cbfe964577d2e8116f7c917f5a3234f7638a
Darkom-svg <dzurilla.marco@gmail.com>
2025-03-30 21:50:51 +0200

    Change image layout

--- src/components/sections/gallery_component.php
+++ src/components/sections/gallery_component.php
@@ -2,21 +2,31 @@
 	<div class="section_header" onclick="scrollToAndToggleOn('gallery')">
 		<h2>Galéria</h2>
 	</div>
-	<div id="gallery_content" class="toggle-content hide content p-5 sm:p-8">
+	<div id="gallery_content">
 		<div class="gallery_section">
 			<div class="header">
 				<h1>Letné sústredenie 2024</h1>
 			</div>
-			<div class="content">
-				<img src="gallery/burns.jpg">
-				<img class="h max-md:hidden md:invisible" src="gallery/burns.jpg">
-				<img src="gallery/image.jpg">
-				<img class="h max-md:hidden md:invisible" src="gallery/image.jpg">
-				<img src="gallery/grill.jpg">
-				<img src="gallery/image2.jpg">
-				<img src="gallery/sousages.jpg">
-				<img src="gallery/playing.jpg">
-			</div>
+			<div class="content grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-4">
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/burns.jpg" alt="">
+				</div>
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/sousages.jpg" alt="">
+				</div>
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/grill.jpg" alt="">
+				</div>
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/playing.jpg" alt="">
+				</div>
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/image.jpg" alt="">
+				</div>
+				<div>
+					<img class="h-auto max-w-full rounded-lg" src="gallery/image2.jpg" alt="">
+				</div>
+			</div>	
 		</div>
 	</div>
 </section>
